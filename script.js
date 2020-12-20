b47b3e9893f82454386cc117c9b87c96e934c22f
turtles <me@ramon.nyc>
2020-12-19 20:57:44 -0800

    simlified div creation

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.getElementById("submitBtn").addEventListener("click",function(event) {
     event.preventDefault();
         //Create a city object from submission
         var cityName = citySearch.value.trim();
-        var cityDivname = citySearch.value.trim();
         //Check function is not b a city object from submission
         if(cityName == ""){
           alert("Enter a city!");
@@ -73,9 +72,6 @@ $.ajax({
       });
   });
 
-
-
-
   // Here we run our AJAX call to the OpenWeatherMap API for the 5 day forecast
 $.ajax({
   url: queryURL2,
@@ -86,33 +82,34 @@ $.ajax({
 
     // Converting dt - SOURCE CODE from above
     var tsF = response2.list[10].dt;
-    // Convert unix timestamp to milliseconds 
-    var tsF_ms = tsF * 1000;
-    // initialize new Date object
-    var date_ob = new Date(tsF_ms);
-    // year as 4 digits (YYYY)
-    var year = date_ob.getFullYear().toString().substr(-2);
-    // month as 2 digits (MM)
-    var month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-    // date as 2 digits (DD)
-    var date = ("0" + date_ob.getDate()).slice(-2);
-    // date as MM/DD/YY format
-    var t2 = " " + month + "/" + date+ "/" + year + " ";
+    var tsF_ms = tsF * 1000;                                    // Convert unix timestamp to milliseconds
+    var date_ob = new Date(tsF_ms);                             // initialize new Date object
+    var year = date_ob.getFullYear().toString().substr(-2);     // year as 4 digits (YYYY)
+    var month = ("0" + (date_ob.getMonth() + 1)).slice(-2);     // month as 2 digits (MM)
+    var date = ("0" + date_ob.getDate()).slice(-2);             // date as 2 digits (DD)
+    var t2 = " " + month + "/" + date+ "/" + year + " ";        // date as MM/DD/YY format
 
-    // Transfer date content to HTML
-    $("#day2date").text(t2);
-
-    // Convert the temp to fahrenheit and transfer content to HTML
+    // Prepare fahrenheit for transfer
     var tempF = (response2.list[10].main.temp - 273.15) * 1.80 + 32;
-    $("#day2temp").text(tempF.toFixed(1));
-    
-    // Transfer humidity content to HTML
-    $("#day2humi").text(response2.list[10].main.humidity.toFixed(0)+ "%");
 
-    // Transfer icon to HTML
+    // Prepare icon for transfer
     var dayIcon = response2.list[10].weather[0].icon;
     var iconLocation = "http://openweathermap.org/img/wn/" + dayIcon + "@2x.png";
+
+    // Prepare variables for transfer append
+    var forcastDate = $("<p></p>").text(t2);
+    var forcastIcon = $("<a></a>").html('<img src="' + iconLocation +'" </img>');
+    var forcastTemp = $("<p></p>").text("Temperature: " + tempF.toFixed(1) + " deg");
+    var forcastHumi = $("<p></p>").text("Humidity: " + response2.list[10].main.humidity.toFixed(0)+ "%");
+    var newDiv = $("<div></div>").append(forcastDate, forcastIcon, forcastTemp, forcastHumi);
+    $("#addHere").append(newDiv).addClass("col card bg-primary text-white");
+/*
+    // Transfer content to HTML
+    $("#day2date").text(t2);
+    $("#day2temp").text(tempF.toFixed(1));
+    $("#day2humi").text(response2.list[10].main.humidity.toFixed(0)+ "%");
     $("#day2icon").html('<img src="' + iconLocation +'" </img>');
+*/
 
   });
 
